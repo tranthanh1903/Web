df1caa7c87abb3c4fe18fa5c59d8b706eb28399f
thanhtran <thanhtranthi1903@gmail.com>
2021-08-30 11:53:05 +0700

    push code

--- day8/asset/day8.js
+++ day8/asset/day8.js
@@ -1,5 +1,9 @@
 const labels = document.querySelectorAll('label')
 
 labels.forEach(label => {
-    label.innerHTML = label.innerText.split('').map((value, i) => `<p style="transition-delay:${i * 70}ms">${value}</p>`).join('')
+    let a = label.innerHTML
+    a = label.innerText.split('')
+    a = a.map((value, i) => `<p style="transition-delay:${i * 70}ms">${value}</p>`)
+    a = a.join('')
+    label.innerHTML = a;
 })
