322c9464586051c566c73b044f598117033dfde4
thanhtran <thanhtranthi1903@gmail.com>
2021-08-30 11:55:12 +0700

    push

--- day8/asset/day8.js
+++ day8/asset/day8.js
@@ -1,8 +1,7 @@
 const labels = document.querySelectorAll('label')
 
 labels.forEach(label => {
-    let a = label.innerHTML
-    a = label.innerText.split('')
+    let a = label.innerText.split('')
     a = a.map((value, i) => `<p style="transition-delay:${i * 70}ms">${value}</p>`)
     a = a.join('')
     label.innerHTML = a;
