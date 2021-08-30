f68bd19f9c9447b983bc271b51ced13e3a0b6a79
thanhtran <thanhtranthi1903@gmail.com>
2021-08-30 11:10:41 +0700

    push

--- day8/asset/day8.js
+++ day8/asset/day8.js
@@ -1,4 +1,4 @@
-const labels = document.querySelectorAll('.form-control label')
+const labels = document.querySelectorAll('label')
 
 labels.forEach(label => {
     label.innerHTML = label.innerText.split('').map((value, i) => `<p style="transition-delay:${i * 70}ms">${value}</p>`).join('')
